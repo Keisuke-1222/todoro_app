dc18f8708691836cbba2c6138e8195a5115d841a
keisuke-1222 <s.k.s.k.ra20@gmail.com>
2018-10-22 13:33:19 +0900

    Fix task_index_spec to refactoring

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, name: 'user_a', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'user_b', email: 'b@example.com') }
+
     before do
-      user_a = FactoryBot.create(:user, name: 'user_a', email: 'a@example.com')
       FactoryBot.create(:task, name: 'first task', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'when user_a logged in' do
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_a }
 
       it 'display task belongs to user_a' do
         expect(page).to have_content 'first task'
@@ -21,13 +22,7 @@ describe 'タスク管理機能', type: :system do
     end
 
     context 'when user_b logged in' do
-      before do
-        FactoryBot.create(:user, name: 'user_b', email: 'b@example.com')
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_b }
 
       it 'not display task belongs to user_a' do
         expect(page).to have_no_content 'first task'
