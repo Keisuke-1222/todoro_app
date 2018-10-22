58779e0e097df1b7357c349fb517246dd9f60487
keisuke-1222 <s.k.s.k.ra20@gmail.com>
2018-10-22 14:05:36 +0900

    Add spec in tasks_spec

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
-describe 'タスク管理機能', type: :system do
-  describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'user_a', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'user_b', email: 'b@example.com') }
+describe 'task managemant function', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'user_a', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'user_b', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: 'first task', user: user_a) }
 
-    before do
-      FactoryBot.create(:task, name: 'first task', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
 
+  shared_examples_for 'display task belongs to user_a' do
+    it { expect(page).to have_content 'first task' }
+  end
+
+  describe 'task index' do
     context 'when user_a logged in' do
       let(:login_user) { user_a }
 
-      it 'display task belongs to user_a' do
-        expect(page).to have_content 'first task'
-      end
+      it_behaves_like 'display task belongs to user_a'
     end
 
     context 'when user_b logged in' do
@@ -26,6 +28,45 @@ describe 'タスク管理機能', type: :system do
 
       it 'not display task belongs to user_a' do
         expect(page).to have_no_content 'first task'
+      end
+    end
+  end
+
+  describe 'task show' do
+    context 'when user_a logged in' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it_behaves_like 'display task belongs to user_a'
+    end
+  end
+
+  describe 'task create' do
+    let(:login_user) { user_a }
+    let(:task_name) { 'task_1' }
+
+    before do
+      visit new_task_path
+      fill_in '名称', with: task_name
+      click_button '登録する'
+    end
+
+    context 'when task_name is inputed' do
+      it 'normally registered' do
+        expect(page).to have_selector '.alert-success', text: 'task_1'
+      end
+    end
+
+    context 'when task_name is not inputed' do
+      let(:task_name) { '' }
+
+      it 'returns errors' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
       end
     end
   end
